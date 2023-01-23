eab92cf077443a5ced23b0a60a93158ce2fa8232
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2023-01-23 17:35:24 +0900

    UIコンポーネントのmantineを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import React from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </ClerkProvider>
   );
 };
