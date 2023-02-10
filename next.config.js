3be85380e822a2e1a93d1ac6e104ba62a1c7a2da
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2023-02-11 05:48:19 +0900

    pwa化 buildエラー修正

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = withPWA({
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
   // images: {
   //   remotePatterns: [
   //     {
