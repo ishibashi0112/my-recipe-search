5430855ab7c6dc96941dcb32e7d2cffbd0f5098e
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2023-02-19 21:18:41 +0900

    SEO対策

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,30 @@ import "@/styles/globals.css";
 
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 
 import { AuthProvider } from "@/pages-component/login/AuthProvider";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-      }}
-    >
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </MantineProvider>
+    <>
+      <Head>
+        <title>my-recipe-search</title>
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </MantineProvider>
+    </>
   );
 };
 
